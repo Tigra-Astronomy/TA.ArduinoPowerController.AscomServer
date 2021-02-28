b0ad166f76ac55731600b783d12a67a30df95ee3
InukPhysiker <InukPhysiker@gmail.com>
2021-02-28 09:41:53 -0600

    update packages

--- TA.ArduinoPowerController.Server/ServerStatusDisplay.cs
+++ TA.ArduinoPowerController.Server/ServerStatusDisplay.cs
@@ -1,6 +1,6 @@
 // This file is part of the TA.ArduinoPowerController project
 //
-// Copyright © 2016-2017 Tigra Astronomy, all rights reserved.
+// Copyright ï¿½ 2016-2017 Tigra Astronomy, all rights reserved.
 // Licensed under the MIT license, see http://tigra.mit-license.org/
 //
 // File: ServerStatusDisplay.cs  Last modified: 2017-03-16@23:34 by Tim Long
